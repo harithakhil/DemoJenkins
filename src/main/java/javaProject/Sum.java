3759719deb532642ee01d2e6f3af263a4f67c435
harithakhil <harithaharidas4134@gmail.com>
2023-04-19 21:45:27 +0530

    first commit

---
+++ src/main/java/javaProject/Sum.java
@@ -0,0 +1,18 @@
+package javaProject;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int a=10,b=5,c;
+		int add=a+b;
+		int sub=a-b;
+		int mul=a*b;
+		int div=a/b;
+		System.out.println("add= "+add);
+		System.out.println("sub= "+sub);
+		System.out.println("mul= "+mul);
+		System.out.println("div= "+div);
+
+	}
+
+}
